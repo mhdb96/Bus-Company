ac52954695f645f4590878bed410c7e6f7d4769a
Talha AYDIN <talhaydn58@gmail.com>
2020-03-23 18:00:09 +0300

    test

--- TASLibrary/Models/TripModel.cs
+++ TASLibrary/Models/TripModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using TASLibrary.CustomDataStructures;
 
 namespace TASLibrary.Models
 {
@@ -16,5 +19,14 @@ namespace TASLibrary.Models
         public decimal SeatPrice { get; set; }
         //public int Seats { get; set; }
 
+        public CLinkedList<TripModel> z { get; set; }
+        public void XMLKaydet()
+        {
+            var xml = new XmlSerializer(typeof(TripModel));
+            using (StreamWriter sw = new StreamWriter(@"C:\\Users\\Talha\\source\\repos\\Bus-Company\\trips.txt"))
+            {
+                xml.Serialize(sw, this);
+            }
+        }
     }
 }
